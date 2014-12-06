27baf217de38e15ab4560592352fe590e8f7cb5c
bioteck <bioteck@hotmail.fr>
2014-12-06 16:59:02 +0100

    Fixes isAbsolute() for Windows ; applies urlProcess option on absolute urls

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ var through = require('through2');
 var isAbsolute = function(p) {
     var normal = path.normalize(p);
     var absolute = path.resolve(p);
-    return normal == absolute;
+
+    if (process.platform === 'win32') {
+        absolute = absolute.replace(/^[a-z]:/i, '');
+    }
+    return normal === absolute;
 },
 isURL = function(url){
     var testPart = url.substr(0,6);
@@ -18,23 +22,25 @@ isURL = function(url){
 var rebaseUrls = function(css, options) {
     return rework(css)
         .use(rework.url(function(url){
-            if (isAbsolute(url) || isURL(url)) {
+            if (/^(data:.*;.*,)/.test(url) || isURL(url)) {
                 return url;
             }
-            if (/^(data:.*;.*,)/.test(url)) {
-                return url;
+            if (isAbsolute(url)) {
+                var p = url;
             }
-            var absolutePath = path.join(options.currentDir, url)
-            var p = path.relative(options.root, absolutePath);
+            else {
+                var absolutePath = path.join(options.currentDir, url)
+                var p = path.relative(options.root, absolutePath);
 
-            if (process.platform === 'win32') {
-                p = p.replace(/\\/g, '/');
-            }
-            if (options.convertToAbsolute) {
-                p = "/" + p;
+                if (process.platform === 'win32') {
+                    p = p.replace(/\\/g, '/');
+                }
+                if (options.convertToAbsolute) {
+                    p = "/" + p;
+                }
             }
             if (typeof options.urlProcess === 'function') {
-                return options.urlProcess(p);
+                return options.urlProcess(p, isAbsolute);
             }
             return p;
         }))
